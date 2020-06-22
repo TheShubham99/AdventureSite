73a1184cff0510942fcecc1cb6398df40f51813e
unknown <ssahasrabhojane@gmail.com>
2020-06-22 23:09:31 +0530

    Updates

--- precache-manifest.83a9a885a88e327db500e13ec8b8f582.js
+++ precache-manifest.83a9a885a88e327db500e13ec8b8f582.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d5cc7bcc133febf4c43486689e8ef87d",
+    "revision": "8e2c28ef53a8b8f9687b77ee984c2bcd",
     "url": "/AdventureSite/index.html"
   },
   {
-    "revision": "0023cf0571074cf7db5b",
+    "revision": "854dd94ead4b801f4563",
     "url": "/AdventureSite/static/css/main.000b3873.chunk.css"
   },
   {
-    "revision": "e51d76d60c3d92f078a2",
-    "url": "/AdventureSite/static/js/2.d554fad5.chunk.js"
+    "revision": "230c48df160ea1b48f49",
+    "url": "/AdventureSite/static/js/2.3c6e1f56.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/AdventureSite/static/js/2.d554fad5.chunk.js.LICENSE.txt"
+    "url": "/AdventureSite/static/js/2.3c6e1f56.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0023cf0571074cf7db5b",
-    "url": "/AdventureSite/static/js/main.74ddb6a2.chunk.js"
+    "revision": "854dd94ead4b801f4563",
+    "url": "/AdventureSite/static/js/main.e4b02a7c.chunk.js"
   },
   {
     "revision": "56d40c3dd4ec3ebeebb6",
